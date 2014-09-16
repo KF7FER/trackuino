89594afd155ffbd008f8c5967f232f1127bc3664
kf7fer <bradb667@gmail.com>
2014-09-15 20:13:57 -0700

    Added support for uBLOX MAX-6/7/8 GPS

--- trackuino/gps.h
+++ trackuino/gps.h
@@ -36,6 +36,8 @@ extern char gps_aprs_lon[10];
 extern float gps_course;
 extern float gps_speed;
 extern float gps_altitude;
+extern uint8_t gps_num_sats;
+extern bool gps_low_power_mode;
 
 void gps_setup();
 bool gps_decode(char c);
